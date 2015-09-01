d77e0bd446513cd276d4a764cebefd48626685ee
Petr <petr@kle.cz>
2015-09-01 06:16:12 +0200

    window.location.replace - fix back button in browser

--- mc.js
+++ mc.js
@@ -5,7 +5,7 @@ var disturlver = /^http:\/\/search\.cpan\.org\/~[^\/]*\/(.*)-([0-9\.]+)\/((.*)\.
 if(url.match(disturlver)){
 	var dist = url.replace(disturlver, '$1')
 	dist = dist.replace(/-/g, '::')
-	window.location='https://metacpan.org/pod/' + dist
+	window.location.replace('https://metacpan.org/pod/' + dist)
 }
 
 var disturl = /^http:\/\/search\.cpan\.org\/dist\/(.*)$/
@@ -13,12 +13,12 @@ var disturl = /^http:\/\/search\.cpan\.org\/dist\/(.*)$/
 if(url.match(disturl)){
 	var dist = url.replace(disturl, '$1')
 	dist = dist.replace(/-/g, '::')
-	window.location='https://metacpan.org/pod/' + dist
+	window.location.replace('https://metacpan.org/pod/' + dist)
 }
 
 var authorurl = /^http:\/\/search\.cpan\.org\/~([^\/]*)\/$/
 
 if(url.match(authorurl)){
 	var author = url.replace(authorurl, '$1').toUpperCase();
-	window.location='https://metacpan.org/author/' + author
+	window.location.replace('https://metacpan.org/author/' + author)
 }
